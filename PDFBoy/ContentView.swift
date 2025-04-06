20f072de1ad7d5f74da25a638c7d8934bf04b29e
Elian_Olart_Soft <jernelx7@gmail.com>
2025-04-05 18:44:27 -0600

    Initial Commit

---
+++ PDFBoy/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PDFBoy
+//
+//  Created by Elian Hernández Olarte on 05/04/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PDFBoy/PDFBoyApp.swift
@@ -0,0 +1,17 @@
+//
+//  PDFBoyApp.swift
+//  PDFBoy
+//
+//  Created by Elian Hernández Olarte on 05/04/25.
+//
+
+import SwiftUI
+
+@main
+struct PDFBoyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
